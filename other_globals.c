5dd1f5290059059a30f2cda57883eddc1570e744
Esteban Zimanyi <estebanzimanyi@gmail.com>
2024-09-28 22:04:30 +0200

    Update list of PostgreSQL and PostGIS globals

--- other_globals.c
+++ other_globals.c
@@ -4,39 +4,8 @@
 
 /* File /meos/postgres/pg_config.h */
 
-/* Define to 1 if you have the global variable 'int opterr'. */
-#define HAVE_INT_OPTERR 1
-
-/* Define to 1 if you have the global variable 'int optreset'. */
-/* #undef HAVE_INT_OPTRESET */
-
 /* Define to 1 if you have the global variable 'int timezone'. */
 #define HAVE_INT_TIMEZONE 1
-
-/* Define to 1 if you have the global variable
-   'rl_completion_append_character'. */
-#define HAVE_RL_COMPLETION_APPEND_CHARACTER 1
-
-/* Define to 1 if you have the global variable 'rl_completion_suppress_quote'.
-   */
-#define HAVE_RL_COMPLETION_SUPPRESS_QUOTE 1
-
-/* Define to 1 if you have the global variable 'rl_filename_quote_characters'.
-   */
-#define HAVE_RL_FILENAME_QUOTE_CHARACTERS 1
-
-/* Define to 1 if you have the global variable 'rl_filename_quoting_function'.
-   */
-#define HAVE_RL_FILENAME_QUOTING_FUNCTION 1
-
-/*****************************************************************************/
-
-/* File /postgres/utils/datetime.h */
-
-/* Definitions of the global variables taken from miscadmin.h */
-extern int DateStyle;
-extern int DateOrder;
-extern int IntervalStyle;
 
 /*****************************************************************************/
 
@@ -94,78 +63,10 @@ static int  DCHCounter = 0;    /* aging-event counter */
  * PostGIS Globals 
  *****************************************************************************/
 
-/* File /postgis/liblwgeom/liblwgeom.h */
+/* File /postgis/libpgcommon/lwgeom_transform.c */
 
-/**
- * Global functions for memory/logging handlers.
- */
-typedef void* (*lwallocator)(size_t size);
-typedef void* (*lwreallocator)(void *mem, size_t size);
-typedef void (*lwfreeor)(void* mem);
-typedef void (*lwreporter)(const char* fmt, va_list ap)
-  __attribute__ (( format(printf, 1, 0) ));
-typedef void (*lwdebuglogger)(int level, const char* fmt, va_list ap)
-  __attribute__ (( format(printf, 2,0) ));
-
-/*****************************************************************************/
-
-/* File /postgis/liblwgeom/lwout_twkb.h */
-
-typedef struct
-{
-	/* Options defined at start */
-	uint8_t variant;
-	int8_t prec_xy;
-	int8_t prec_z;
-	int8_t prec_m;
-	float factor[4]; /*What factor to multiply the coordinates with to get the requested precision*/
-} TWKB_GLOBALS;
-
-/*****************************************************************************/
-
-/* File /postgis/liblwgeom/lwgeom_transform.h */
-
-/*
-* Proj4 caching has it's own mechanism, and is
-* stored globally as the cost of proj_create_crs_to_crs()
-* is so high (20-40ms) that the lifetime of fcinfo->flinfo->fn_extra
-* is too short to assist some work loads.
-*/
-
-/* An entry in the PROJ SRS cache */
-typedef struct struct_PROJSRSCacheItem
-{
-	int32_t srid_from;
-	int32_t srid_to;
-	uint64_t hits;
-	LWPROJ *projection;
-}
-PROJSRSCacheItem;
-
-/* PROJ 4 lookup transaction cache methods */
-#define PROJ_CACHE_ITEMS 128
-
-/*
-* The proj4 cache holds a fixed number of reprojection
-* entries. In normal usage we don't expect it to have
-* many entries, so we always linearly scan the list.
-*/
-typedef struct struct_PROJSRSCache
-{
-	PROJSRSCacheItem PROJSRSCache[PROJ_CACHE_ITEMS];
-	uint32_t PROJSRSCacheCount;
-	MemoryContext PROJSRSCacheContext;
-}
-PROJSRSCache;
-
-/*****************************************************************************/
-
-/* File /postgis/liblwgeom/lwin_wkt.h */
-
-/*
-* Global that holds the final output geometry for the WKT parser.
-*/
-extern LWGEOM_PARSER_RESULT global_parser_result;
+/* Global to hold the Proj object cache */
+PROJSRSCache *PROJ_CACHE = NULL;
 
 /*****************************************************************************/
 
@@ -193,80 +94,22 @@ int wkt_yydebug = 0;
 /* File /postgis/liblwgeom/lwutils.c */
 
 /* Default allocators */
-static void * default_allocator(size_t size);
-static void default_freeor(void *mem);
-static void * default_reallocator(void *mem, size_t size);
 lwallocator lwalloc_var = default_allocator;
 lwreallocator lwrealloc_var = default_reallocator;
 lwfreeor lwfree_var = default_freeor;
 
 /* Default reporters */
-static void default_noticereporter(const char *fmt, va_list ap) __attribute__ ((format (printf, 1, 0)));
-static void default_errorreporter(const char *fmt, va_list ap) __attribute__ ((format (printf, 1, 0)));
 lwreporter lwnotice_var = default_noticereporter;
 lwreporter lwerror_var = default_errorreporter;
 
 /* Default logger */
-static void default_debuglogger(int level, const char *fmt, va_list ap) __attribute__ ((format (printf, 2, 0)));
+
 lwdebuglogger lwdebug_var = default_debuglogger;
 
-#define LW_MSG_MAXLEN 256
-
-static char *lwgeomTypeName[] = {...}
-
-/* Structure for the type array */
-struct geomtype_struct
-{
-	char *typename;
-	int type;
-	int z;
-	int m;
-};
-
-/* Type array. Note that the order of this array is important in
-   that any typename in the list must *NOT* occur within an entry
-   before it. Otherwise if we search for "POINT" at the top of the
-   list we would also match MULTIPOINT, for example. */
-
-struct geomtype_struct geomtype_struct_array[] = {...}
-
 /*****************************************************************************/
-
-/* File /postgis/libpgcommon/lwgeom_pg.h */
-
-typedef enum
-{
-	GEOMETRYOID = 1,
-	GEOGRAPHYOID,
-	BOX3DOID,
-	BOX2DFOID,
-	GIDXOID,
-	RASTEROID,
-	POSTGISNSPOID
-} postgisType;
-
-typedef struct
-{
-	Oid geometry_oid;
-	Oid geography_oid;
-	Oid box2df_oid;
-	Oid box3d_oid;
-	Oid gidx_oid;
-	Oid raster_oid;
-	Oid install_nsp_oid;
-	char *install_nsp;
-	char *spatial_ref_sys;
-} postgisConstants;
 
 /* Global to hold all the run-time constants */
 extern postgisConstants *POSTGIS_CONSTANTS;
-
-/*****************************************************************************/
-
-/* File /postgis/libpgcommon/lwgeom_transform.c */
-
-/* Global to hold the Proj object cache */
-PROJSRSCache *PROJ_CACHE = NULL;
 
 /*****************************************************************************/
 
