4583d3086625e3836c7b9cf0789193cb35190936
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-04-30 12:24:09 +0200

    fix(803): revert text2cstring → text_to_cstring (matches #751 pgtypes API)

--- mobilitydb/src/temporal/type_in.c
+++ mobilitydb/src/temporal/type_in.c
@@ -103,7 +103,7 @@ Datum
 Temporal_from_mfjson(PG_FUNCTION_ARGS)
 {
   text *mfjson_txt = PG_GETARG_TEXT_P(0);
-  char *mfjson = text2cstring(mfjson_txt);
+  char *mfjson = text_to_cstring(mfjson_txt);
   MeosType temptype = oid_meostype(get_fn_expr_rettype(fcinfo->flinfo));
   Temporal *result = temporal_from_mfjson(mfjson, temptype);
   pfree(mfjson);
