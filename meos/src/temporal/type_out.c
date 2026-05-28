5b6a14d1a87db108c755e8c08ffee4988cd59ef5
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-05-28 09:29:46 +0200

    Remove duplicate cbuffer_as_json_sb in type_out.c MFJSON union

--- meos/src/temporal/type_out.c
+++ meos/src/temporal/type_out.c
@@ -327,28 +327,6 @@ tpcpatch_as_mfjson_sb(stringbuffer_t *sb, const TInstant *inst, int precision)
   stringbuffer_append_len(sb, "]}", 2);
 }
 #endif /* POINTCLOUD */
-#if CBUFFER
-/**
- * @brief Write into the buffer a circular buffer in the MF-JSON representation
- */
-static void
-cbuffer_as_json_sb(stringbuffer_t *sb, const Cbuffer *cb, int precision)
-{
-  assert(precision <= OUT_MAX_DOUBLE_PRECISION);
-  GSERIALIZED *gs = cbuffer_point(cb);
-  const POINT2D *pt = GSERIALIZED_POINT2D_P(gs);
-  stringbuffer_append_len(sb, "{\"point\":[", 10);
-  stringbuffer_append_double(sb, pt->x, precision);
-  stringbuffer_append_char(sb, ',');
-  stringbuffer_append_double(sb, pt->y, precision);
-  stringbuffer_append_len(sb, "],\"radius\":", 11);
-  stringbuffer_append_double(sb, cbuffer_radius(cb), precision);
-  stringbuffer_append_char(sb, '}');
-  pfree(gs);
-  return;
-}
-#endif /* CBUFFER */
-
 #if POSE || RGEO
 /**
  * @brief Write into the buffer a pose in the MF-JSON representation
